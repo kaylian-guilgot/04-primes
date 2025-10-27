1fc6ef52013d335c8c2f1991c6204c69974f4329
kaylian-guilgot <kaylian.guilgot@edu.esiee.fr>
2025-10-27 15:49:28 +0000

    Modification de la fonction isprime et main

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
+"""Programme affichant les nombres premiers de 0 à 100."""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
-
-    # votre code ici
-
-    pass
+    """Renvoie True si p est premier, sinon False"""
+    if p in (1, 0):
+        return False
+    if p==2:
+        return True
+    for d in range(2, int(sqrt(p))+1):
+        if p%d == 0:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
-
+    """Affiche les nombres premiers de 0 à 100"""
     # vos appels à la fonction secondaire ici
-
-    for n in range(100):
+    borne_droite = 100
+    print(f"Les nombres premiers de 0 à {borne_droite} sont : ")
+    for n in range(borne_droite):
         if isprime(n):
             print(n, end=", ")
-
-    print()
-
 
 if __name__ == "__main__":
     main()
